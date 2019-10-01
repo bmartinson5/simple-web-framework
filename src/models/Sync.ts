b635637b3715890731f6d85cd8d9434e0075cb0c
Student <example@example.com>
2019-10-01 16:44:47 -0700

    added sync class to handle api calls

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,25 +1,23 @@
 import axios, { AxiosResponse, AxiosPromise } from "axios";
-import { UserProps } from './User'
 
-export class Sync {
 
+interface HasId {
+    id?: number;
+}
+
+export class Sync<T extends HasId> {
     constructor(public rootUrl: string){};
 
     fetch(id: number): AxiosPromise {
         return axios.get(`${this.rootUrl}/${id}`);
     }
 
-    save(data: UserProps):void {
+    save(data: T): AxiosPromise {
         const {id} = data;
         if(id){
-            axios.put(`${this.rootUrl}/${id}`, data)
-                .then((response: AxiosResponse): void => {
-            }); 
+            return axios.put(`${this.rootUrl}/${id}`, data);
         } else {
-            axios.post(`${this.rootUrl}/`, data)
-                .then((response: AxiosResponse): void => {
-                    this.set(response.data);
-            }); 
+            return axios.post(`${this.rootUrl}/`, data);
         }
     }
 }
