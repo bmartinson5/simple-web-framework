5efc94593039376321ab2d052f4d7e6feb3dc760
Benjamin Martinson <benmartinson92@gmail.com>
2019-10-05 15:15:45 -0700

    user model can save and fetch

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,19 @@
 import {User} from './models/User';
 
-import axios from 'axios';
 
-class Person {
-    constructor(public firstName: string, public lastName: string){
 
-    }
-
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`
-    }
-
-}
-
-const person = new Person('firstName', 'lastName');
-console.log(person.fullName);
-
-const user = new User({name: 'name', age: 0});
-console.log(user.get('name'));
+const user = new User({id: 2});
 
 const on = user.on;
 const trigger = user.trigger;
 
-on('change', () => {
-    console.log('clicked on');
+on('save', () => {
+    console.log(user);
     
 })
 
-trigger('change');
+user.save();
+
+
 
 
